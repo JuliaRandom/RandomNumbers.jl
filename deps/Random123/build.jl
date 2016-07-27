0f3c4fe911d878481e0e77cda3d5d767c2396625
sunoru <s@sunoru.com>
2016-07-27 12:35:59 +0800

    Fix "LLVM ERROR: JIT does not support inline asm!".

--- deps/Random123/build.jl
+++ deps/Random123/build.jl
@@ -9,12 +9,14 @@ function have_aesni()
     if VERSION < v"0.5-"
         return false
     end
-    ecx = Base.llvmcall(
+    @eval begin # use `@eval` to avoid errors while compiling (with julia 0.4)
+        ecx = Base.llvmcall(
         """%1 = call { i32, i32, i32, i32 } asm "xchgq  %rbx,\${1:q}\\0A  cpuid\\0A  xchgq  %rbx,\${1:q}",
         "={ax},=r,={cx},={dx},0,~{dirflag},~{fpsr},~{flags}"(i32 1)
         %2 = extractvalue { i32, i32, i32, i32 } %1, 2
         ret i32 %2""", UInt32, Tuple{})
-    (ecx >> 25) & 1 == 1
+        (ecx >> 25) & 1 == 1
+    end
 end
 
 check_compiler() = success(`gcc --version`)
