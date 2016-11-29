e24994e562d9ad0d6ab0a6c5b52f4fe756bd4bd0
sunoru <sunoru@live.com>
2016-11-29 17:36:32 +0800

    Add `rand_ui52` for some generic functions in `Base.Random`.

--- src/common.jl
+++ src/common.jl
@@ -1,4 +1,4 @@
-import Base.Random: rand
+import Base.Random: rand, rand_ui52
 
 """
 ```julia
@@ -11,11 +11,14 @@ abstract AbstractRNG{T<:Number} <: Base.Random.AbstractRNG
 
 typealias BitTypes Union{Bool, Signed, Unsigned}
 
+@inline function rand_ui52(rng::AbstractRNG)
+    rand(rng, UInt64) & Base.significand_mask(Float64)
+end
+
 # see https://github.com/sunoru/RNG.jl/issues/8
 # TODO: find a better approach.
 @inline function rand{T<:Union{UInt64, UInt128}}(rng::AbstractRNG{T}, ::Type{Float64}=Float64)
-    u = rand(rng, UInt64)
-    reinterpret(Float64, Base.exponent_one(Float64) | Base.significand_mask(Float64) & u) - 1.0
+    reinterpret(Float64, Base.exponent_one(Float64) | rand_ui52(rng)) - 1.0
 end
 @inline function rand{T<:Union{UInt8, UInt16, UInt32}}(rng::AbstractRNG{T}, ::Type{Float64}=Float64)
     rand(rng, T) * exp2(-sizeof(T) << 3)
