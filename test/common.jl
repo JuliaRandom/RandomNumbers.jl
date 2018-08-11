02d63f471c8015671c6bdb9d172e0a231bd9dc63
Michael J. Curry <curry@cs.umd.edu>
2018-08-10 22:03:58 -0400

    test fixes (hopefully)

--- test/common.jl
+++ test/common.jl
@@ -1,7 +1,7 @@
 using RandomNumbers
 import Random: seed!
 using Test: @test
-using Printf: @printf
+using Base.Printf: @printf
 
 function compare_dirs(dir1::AbstractString, dir2::AbstractString)
     files1 = readdir(dir1)
